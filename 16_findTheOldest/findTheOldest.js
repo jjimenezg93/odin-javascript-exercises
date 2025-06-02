67ba6c00b721c4a7fbf23dc43d312d78f62b51d8
Julián Jiménez <jjimenezg93@gmail.com>
2025-06-02 13:45:09 +0100

    Add solution for 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+function getAge(person) {
+  let endYear = person.yearOfDeath ?? new Date().getFullYear();
+  return endYear - person.yearOfBirth;
+}
 
+const findTheOldest = function (array) {
+  return array.reduce((a, b) => (getAge(a) > getAge(b) ? a : b));
 };
 
 // Do not edit below this line
